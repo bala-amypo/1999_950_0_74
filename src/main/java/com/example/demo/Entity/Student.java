82dcca21edb6cd9ecaa353477d6755986af1ce26
bala-amypo <bala@amypo.in>
2025-12-17 09:39:21 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Student.java
+++ src/main/java/com/example/demo/Entity/Student.java
@@ -1,37 +1,40 @@
-public com .example.demo.Entity;
+package com.example.sql.Entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import.persistence.GenerationType;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entitypublic class Student{
+@Entity
+public class Student {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
-    private int getId(){
+    public int getId() {
         return id;
     }
-    public void SetId(Integer id){
-        this.id=id;
+    public void setId(Integer id) {
+        this.id = id;
     }
-    public void setName(String name){
-        this.name=name;
+    public String getName() {
+        return name;
     }
-    public String getEmail(){
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
         return email;
     }
-    public void setEmail(String email){
-        this.email=email;
+    public void setEmail(String email) {
+        this.email = email;
     }
-    public Student(Integer id,String name,String email){
-        this.id=id;
-        this.name=name;
-        this.email=email;
+    public Student(Integer id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
     }
-    public Student(){
-
+    public Student() {
     }
 }
--- src/main/java/com/example/demo/Repository/StudentRepo.java
+++ src/main/java/com/example/demo/Repository/StudentRepo.java
@@ -1,8 +1,22 @@
-package com.example.demo.Repository;
-import org.springframework.web.bind.annotation.;
-importorg.springframework.data.jpa.repository.jpaRepository;
+// package com.example.demo.Repository;
+// import org.springframework.web.bind.annotation.;
+// importorg.springframework.data.jpa.repository.jpaRepository;
+// import org.springframework.stereotype.Repository;
+// @Repository
+// public interface StudentRepo extends jpaRepository<Student,Integer>{
+
+// }
+
+
+package com.example.sql.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.sql.Entitydata.Student;
+
 @Repository
-public interface StudentRepo extends jpaRepository<Student,Integer>{
+public interface StudentRepo extends JpaRepository<Student,Integer> {
+
     
 }
