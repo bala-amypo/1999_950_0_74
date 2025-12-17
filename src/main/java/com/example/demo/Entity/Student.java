006bea82acdcad208bc0f3781af604dce987bba9
bala-amypo <bala@amypo.in>
2025-12-17 08:44:43 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Student.java
+++ src/main/java/com/example/demo/Entity/Student.java
@@ -7,5 +7,31 @@ import jakarta.persistence.Id;
 
 @Entitypublic class Student{
     @Id
-    @GeneratedValue(s)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+    private int getId(){
+        return id;
+    }
+    public void SetId(Integer id){
+        this.id=id;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public Student(Integer id,String name,String email){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+    }
+    public Student(){
+
+    }
 }
---
+++ src/main/java/com/example/demo/Repository/StudentRepo.java
@@ -0,0 +1,5 @@
+package com.example.demo.Repository;
+import org.springframework.web.bind.annotation.;
+importorg.springframework.data.jpa.repository.jpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
